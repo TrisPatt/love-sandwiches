26f7706e897f8d6cec8aecf0032e7211118f7343
Tristan Pattison <tristanpattison@yahoo.co.uk>
2024-04-08 13:04:08 +0000

    add validaData function

--- run.py
+++ run.py
@@ -20,10 +20,20 @@ def getSalesData():
     """
     print("Please enter sales data")
     print("Data should be six numbers, seperated by comma's")
-    print("Eg 3,4,5,6,7,8"/n)
+    print("Eg 3,4,5,6,7,8\n")
 
-    get_str= input("Enter your data here")
-    print(f"The data provided is {get_str}")
+    data_str= input("Enter your data here: ")
+    
+    salesData = data_str.split(",")
+    validateData(salesData)
+
+
+def validateData(values):
+    
+    try:
+        if len(values) != 6:
+            raise ValueError(f"exactly 6 values required, you provided {len(values)}")
+    except ValueError as e:
+        print(f"Invalid data: {e} Please try again \n")
 
 getSalesData()
-
